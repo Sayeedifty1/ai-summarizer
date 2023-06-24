8e75d8a8dec3ca7123db6eb52d89d933fe9a37d6
Sayeedifty1 <md.abusayeedifty@gmail.com>
2023-06-25 01:43:10 +0600

    hero created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-
+import './App.css';
+import Hero from './Components/Hero';
+import Demo from './components/Demo';
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+      <div className="main">
+        <div className="gradient"/>
+        
+        <div className="app">
+          <Hero></Hero>
+          <Demo></Demo>
+        </div>
+      </div>
+    </main>
   )
 }
 
