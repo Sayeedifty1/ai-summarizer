93bfdba48243edad686e62fb2785d648ceea791e
Sayeedifty1 <md.abusayeedifty@gmail.com>
2023-06-26 01:15:26 +0600

    article section added

--- src/Components/Demo.jsx
+++ src/Components/Demo.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { copy, linkIcon, loader, tick } from '../assets';
 
 import { useLazyGetSummaryQuery } from '../Services/article';
@@ -11,17 +11,25 @@ const Demo = () => {
   const [allArticles, setAllArticles] = useState([]);
 
   const [getSummary, { isFetching, error }] = useLazyGetSummaryQuery();
+  const [copied , setCopied] = useState("");
 
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'));
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
+    }
+
+  }, [])
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const { data } = await getSummary({articleUrl: article.url});
+    const { data } = await getSummary({ articleUrl: article.url });
 
     if (data?.summary) {
       const newArticle = { ...article, summary: data.summary };
-      const updatedAllArticles = [newArticle, ...allArticles ];
+      const updatedAllArticles = [newArticle, ...allArticles];
       setArticle(newArticle);
-      setAllArticles(updatedAllArticles); 
-      console.log(newArticle);
+      setAllArticles(updatedAllArticles);
+      localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
     }
   };
 
@@ -29,6 +37,15 @@ const Demo = () => {
     setArticle({ ...article, url: e.target.value });
   };
 
+  const copyToClipboard = (copyUrl) => {
+    setCopied(copyUrl);
+  
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  };
+  console.log( copied)
   return (
     <section className='mt-16 w-full max-w-xl'>
       <div className='flex flex-col w-full gap-2'>
@@ -47,8 +64,48 @@ const Demo = () => {
           </button>
         </form>
         {/* Browse url history */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((article, index) => (
+            <div key={`link-${index}`}
+              onClick={() => setArticle(article)}
+              className="link_card"
+            >
+              <div className="copy_btn" onClick={()=>copyToClipboard(article.url)}>
+                <img src={copied === article.url ? tick : copy}
+                 alt='copy icon'
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+                {article.url}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
       {/* display results */}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
+        ) : error ? (
+          <p className='text-red-500 text-center'>Well that wasn't suppose to happen. <br />
+            <span className='font-satoshi font-normal text-gray-700'>
+              {error?.data?.error || 'Something went wrong'}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className='flex flex-col gap-3'>
+              <h2 className='font-satoshi font-bold text-xl text-gray-600'>Article <span className='blue_gradient'>Summary</span></h2>
+              <div className="summary_box">
+                <p className='font-inter font-medium text-sm text-gray-700'>{article.summary}</p>
+              </div>
+            </div>
+
+          )
+           
+          )}
+      </div>
     </section>
   );
 };
